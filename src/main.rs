3e11a0e5d02d39fe1a2d1f72c1c52d137b579a9f
JatoMixo <jatomixo@proton.me>
2024-02-12 18:44:16 +0100

    Small changes to the server experiment

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,28 @@
 use axum::routing::get;
 use socketioxide::{
-    extract::SocketRef,
+    extract::{
+        SocketRef,
+        Data,
+    },
     SocketIo
 };
 use tracing::info;
+use tracing_subscriber::FmtSubscriber;
+use serde_json::Value;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    tracing::subscriber::set_global_default(FmtSubscriber::default())?;
 
     info!("Starting server");
 
     let (layer, io) = SocketIo::new_layer();
 
     io.ns("/", |socket: SocketRef| {
-        socket.on("message", |message_socket: SocketRef| {
-            info!("Socket connected: {}", message_socket.id);
+        info!("Socket connected: {}", socket.id);
+
+        socket.on("message", |message_socket: SocketRef, Data::<Value>(data)| {
+            info!("Data received: {:?}", data);
             message_socket.emit("message-back", "Hello to you").ok();
         });
     });
